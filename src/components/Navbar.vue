9a9ec08fa27a38d6aee3b98e6b28673ffb59b2f5
ravenusmc <mcuddy77@gmail.com>
2018-11-17 18:33:10 -0500

    added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
