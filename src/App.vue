91d79a7038c26beac729f475fa9d141a8336ab54
ravenusmc <mcuddy77@gmail.com>
2018-11-18 11:28:27 -0500

    added a Header component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <Navbar></Navbar>
+    <Header></Header>
   </div>
 </template>
 
 <script>
   import Navbar from './components/Navbar.vue';
+  import Header from './components/Header.vue';
 
   export default {
     components: {
-      'Navbar': Navbar
+      'Navbar': Navbar,
+      'Header': Header
     }
-    
+
   }
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='header_div'>
+    <div>
+      <h1>The Fall</h1>
+      <hr>
+      <h3>Pictures to bring calmness</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .header_div {
+    height: 500px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
