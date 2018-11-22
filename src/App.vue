2e57f5b6f11e7c1234e49322724e182379bf5bb2
ravenusmc <mcuddy77@gmail.com>
2018-11-22 13:55:25 -0500

    added a footer component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <Navbar></Navbar>
     <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
   import Navbar from './components/Navbar.vue';
   import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
 
   export default {
     components: {
       'Navbar': Navbar,
-      'Header': Header
+      'Header': Header,
+      'Footer': Footer
     }
 
   }
