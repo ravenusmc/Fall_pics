6dc8a449313524933db40bdc705e775557e310be
ravenusmc <mcuddy77@gmail.com>
2018-11-25 14:53:03 -0500

    added in a new controller

--- src/components/home/Center.vue
+++ src/components/home/Center.vue
@@ -1,10 +1,18 @@
 <template lang="html">
   <div>
     <h1 class='center'>The Fall</h1>
-    <section>
-      <div>
+    <section class='top_section'>
 
+      <div>
+        <h3 class='center'><router-link class="nav-link" to="/about">About Me</router-link></h3>
+        <p>Learn more about me and my journey with coding.</p>
       </div>
+
+      <div>
+        <h3 class='center'><router-link class="nav-link" to="/gallery">Gallery</router-link></h3>
+        <p>Experience Some nice fall photo's simply sit back and relax as you click throw them.</p>
+      </div>
+
     </section>
   </div>
 </template>
@@ -15,4 +23,11 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
+  .top_section {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    border: 2px solid blue;
+  }
+
 </style>
